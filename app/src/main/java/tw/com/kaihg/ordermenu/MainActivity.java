56771e4c6c5af31dcb76e12479e0745c440152c4
kaihghuang <kaihg1028@gmail.com>
2016-03-03 16:52:38 +0800

    增加toolbar

--- app/src/main/java/tw/com/kaihg/ordermenu/MainActivity.java
+++ app/src/main/java/tw/com/kaihg/ordermenu/MainActivity.java
@@ -3,13 +3,33 @@ package tw.com.kaihg.ordermenu;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        mToolBar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolBar);
+        mToolBar.inflateMenu(R.menu.main_action_menu);
+        mToolBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_action_menu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
